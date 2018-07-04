f7d8d14b01b983fc937fc44261a035a2b0d12f83
Aaron Krajeski <ak@aaronk.is>
2018-07-04 12:10:25 -0400

    Add loading

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,6 @@
 defmodule Cards do
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "Kin"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
     for suit <- suits, value <- values do
@@ -18,5 +18,17 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, :enoent} -> "That file does not exist"
+    end
   end
 end
